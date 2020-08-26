5e83afe78181ce894ba5a5ef7ec479a04f3ea36f
megobedya <megobedya@debian>
2020-08-26 23:00:13 +0300

    fase 3

--- home_task_20.py
+++ home_task_20.py
@@ -4,3 +4,25 @@ import aiohttp
 import json
 
 
+async def request_data(reddit):
+    url = f'https://www.reddit.com/r/{reddit}/hot.json?limit=5'
+    async with aiohttp.request('GET', url) as resp:
+        return await resp.json()
+
+async def main():
+    reddits = {
+            'python',
+            'compsci', 
+            'microbork',
+        }
+    res = await asyncio.gather(*(request_data(redd) for redd in reddits))
+    data = [x for x  in res] 
+    data_reddits = dict()
+    for i in data:
+        data_reddits[i['data']['children'][0]['data']['subreddit']] = dict()
+        for j in  i['data']['children']:
+            data_reddits[i['data']['children'][0]['data']['subreddit']][j['data']['title']] = {
+                    'score' : j['data']['score'],
+                    'link' : j['data']['permalink']
+                    }
+asyncio.run(main())
